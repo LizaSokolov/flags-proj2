bbd78cd5dfa4ce1e884e1ec3482f49ceb07808c0
LizaSokolov <li.za.sokolova1409@gmail.com>
2024-06-26 20:29:29 +0300

    first commit

---
+++ scripts/localStorage.js
@@ -0,0 +1,24 @@
+let likedCountries = [];
+
+const getData = () => {
+    let data = localStorage.getItem("countries");
+    if (!data) {
+        localStorage.setItem("countries", JSON.stringify(likedCountries));
+        data = localStorage.getItem("countries");
+    }
+    likedCountries = JSON.parse(data);
+}
+//country
+const updateCountries = (countryName) => {
+    if (likedCountries.includes(countryName)) {
+        let filterd = likedCountries.filter((country) => {
+            return country != countryName;
+        });
+        likedCountries = filterd;
+    } else {
+        likedCountries.push(countryName);
+    }
+    localStorage.setItem("countries", JSON.stringify(likedCountries))
+};
+
+export { likedCountries, getData, updateCountries }
